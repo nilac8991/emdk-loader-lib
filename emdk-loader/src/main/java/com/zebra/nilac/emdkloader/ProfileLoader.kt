c32a63dc80e79de2e184c49cd5dd8ead0a356e81
Calin Neamtu <nilac8991@gmail.com>
2023-09-22 11:23:42 +0200

    Add support to process a profile asynchronously
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- emdk-loader/src/main/java/com/zebra/nilac/emdkloader/ProfileLoader.kt
+++ emdk-loader/src/main/java/com/zebra/nilac/emdkloader/ProfileLoader.kt
@@ -1,6 +1,7 @@
 package com.zebra.nilac.emdkloader
 
 import android.util.Log
+import com.symbol.emdk.EMDKBase
 import com.symbol.emdk.EMDKManager
 import com.symbol.emdk.EMDKResults
 import com.symbol.emdk.ProfileManager
@@ -17,6 +18,69 @@ class ProfileLoader {
     private var profileProcessScope = MainScope()
 
     private var mEmdkManager: EMDKManager? = null
+
+    fun processProfileNow(
+        profileName: String,
+        profile: String?,
+        callBacks: ProfileLoaderResultCallback
+    ) {
+        this.mProfileLoaderResultCallback = callBacks
+        mEmdkManager = mEmdkLoaderInstance.getManager()
+
+        if (mEmdkManager == null) {
+            Log.e(TAG, "Unable to process profile, EMDK Manager was not initialised!")
+            mProfileLoaderResultCallback?.onProfileLoadFailed("EMDK not initialised!")
+            return
+        }
+
+        Log.i(TAG, "Initialising ProfileManager asynchronously...")
+        mEmdkManager?.getInstanceAsync(
+            EMDKManager.FEATURE_TYPE.PROFILE,
+            object : EMDKManager.StatusListener {
+                override fun onStatus(statusData: EMDKManager.StatusData, emdkBase: EMDKBase) {
+                    Log.w(TAG, "Status: ${statusData.result}")
+
+                    if (statusData.result != EMDKResults.STATUS_CODE.SUCCESS && statusData.featureType == EMDKManager.FEATURE_TYPE.PROFILE) {
+                        mProfileLoaderResultCallback?.onProfileLoadFailed("Profile Manager is not available!")
+                        return
+                    }
+
+                    val profileManager =
+                        mEmdkManager?.getInstance(EMDKManager.FEATURE_TYPE.PROFILE) as ProfileManager?
+
+                    if (profileManager == null) {
+                        Log.e(TAG, "Profile Manager is not available!")
+                        mProfileLoaderResultCallback?.onProfileLoadFailed("Profile Manager is not available!")
+                        return
+                    }
+
+                    Log.d(TAG, "Processing EMDK profile")
+                    val params = arrayOfNulls<String>(1)
+
+                    if (!profile.isNullOrEmpty()) {
+                        params[0] = profile
+                    }
+
+                    profileProcessScope.launch(Dispatchers.IO) {
+                        profileManager.processProfile(
+                            profileName,
+                            ProfileManager.PROFILE_FLAG.SET,
+                            params
+                        )
+                            .also {
+                                Log.d(TAG, "XML: " + it.statusString)
+
+                                if (it.statusCode == EMDKResults.STATUS_CODE.CHECK_XML || it.statusCode == EMDKResults.STATUS_CODE.SUCCESS) {
+                                    mProfileLoaderResultCallback?.onProfileLoaded()
+                                } else {
+                                    mProfileLoaderResultCallback?.onProfileLoadFailed(it.statusString)
+                                    mProfileLoaderResultCallback?.onProfileLoadFailed(it)
+                                }
+                            }
+                    }
+                }
+            })
+    }
 
     fun processProfile(
         profileName: String,
@@ -38,6 +102,7 @@ class ProfileLoader {
 
         if (profileManager == null) {
             Log.e(TAG, "Profile Manager is not available!")
+            mProfileLoaderResultCallback?.onProfileLoadFailed("Profile Manager is not available!")
             return
         }
 
